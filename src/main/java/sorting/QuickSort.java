c6854d9b26f6bdf6b363315440477d2fa8c13a6e
Student! <tmdflwndtn@gmail.com>
2020-10-05 19:45:06 +0900

    Update QuickSort.java
    퀵정렬 성능과 유사하게 보완

--- src/main/java/sorting/QuickSort.java
+++ src/main/java/sorting/QuickSort.java
@@ -3,21 +3,21 @@ package sorting;
 import java.util.Collections;
 import java.util.Vector;
 public class QuickSort {
-	public void quickSort(Vector<Integer> v, int s, int e) {
-		int pivot = v.get(s);
-		int bs = s, be = e;
-		
-		while(s < e) {
-			while(pivot <= v.get(e) && s < e) e--;
-			if(s > e) break;
-			while(pivot >= v.get(s) && s < e) s++;
-			if(s > e) break;
-			
-			Collections.swap(v, s, e);
-		}
-		Collections.swap(v, bs, s);
-		if(bs < s) quickSort(v, bs, s-1);
-		if(be > e) quickSort(v, s+1, be);
-		
-	}
+	public void quickSort(List<Integer> arr, int s, int e){
+        int left = s;
+        int right = e;
+        int pivot = arr.get((s+e)/2);
+
+        while (left<=right){
+            while (arr.get(left) < pivot) left++;
+            while (arr.get(right) > pivot) right--;
+            if(left <= right)
+                Collections.swap(arr,left++,right--);
+        }
+
+        if(s<right)
+            sort(arr, s, right);
+        if(e>left)
+            sort(arr, left, e);
+    }
 }
