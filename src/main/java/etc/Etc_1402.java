0ae255d5879895435c08f1f7b744634c5ff6ba54
Student! <tmdflwndtn@gmail.com>
2020-10-21 23:53:24 +0900

    백준 문제 - 1402번 구현

---
+++ src/main/java/etc/Etc_1402.java
@@ -0,0 +1,18 @@
+package etc;
+
+import java.util.Scanner;
+
+public class Etc_1402 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int testCaseCount = sc.nextInt();
+
+        for(int t=0;t<testCaseCount;t++){
+            int mainA = sc.nextInt();
+            int primA = sc.nextInt();
+
+            System.out.println("yes");
+
+        }
+    }
+}
